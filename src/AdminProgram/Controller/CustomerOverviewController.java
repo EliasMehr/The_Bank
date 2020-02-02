ac21ef3ac9b086f5af355bfcddb40e95ae9a21c4
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2020-02-02 20:54:09 +0100

    fixat admin-fönster

--- src/AdminProgram/Controller/CustomerOverviewController.java
+++ src/AdminProgram/Controller/CustomerOverviewController.java
@@ -1,4 +1,12 @@
 package AdminProgram.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class CustomerOverviewController {
+    public void addCustomer(MouseEvent mouseEvent) {
+    }
+
+    public void remove(ActionEvent actionEvent) {
+    }
 }
