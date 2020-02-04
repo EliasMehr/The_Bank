434984d0d30093cc309b97422747a6c42321c211
EliasMehr <elias.mehr@gmail.com>
2020-02-04 19:41:23 +0100

    - Added functionality to sign in VIEW - Added functionality to PortalOverView(Fetch data)
    - WIP -> Populate withdrawal selector

--- src/CustomerProgram/Controller/AccountsOverviewController.java
+++ src/CustomerProgram/Controller/AccountsOverviewController.java
@@ -8,17 +8,24 @@ import Model.Transaction;
 import Repository.AccountRepository;
 import Repository.CustomerRepository;
 import Repository.LoanRepository;
+import Repository.TransactionRepository;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import javafx.util.Callback;
 
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class AccountsOverviewController {
@@ -44,11 +51,11 @@ public class AccountsOverviewController {
     @FXML
     private TableView<Transaction> transactionHistory;
     @FXML
-    private TableColumn<Transaction, Date> transactionDateCol;
+    private TableColumn<Transaction, LocalDate> transactionDateCol;
     @FXML
     private TableColumn<Transaction, String> transactionTypeCol;
     @FXML
-    private TableColumn<Transaction, Double> transactionAmountCol;
+    private TableColumn<Transaction, String> transactionAmountCol;
     @FXML
     private TableColumn<Transaction, Integer> transactionAccountCol;
     @FXML
@@ -72,6 +79,15 @@ public class AccountsOverviewController {
         populateAccountsOverview();
         populateLoansOverview();
         populateWithdrawalAccountSelector();
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        TransactionRepository.recentMonthTransactions(customer.getAccounts().get(0));
+
+        transactionAmountCol.setCellValueFactory(transaction -> new SimpleStringProperty(currency.format(transaction.getValue().getAmount())));
+        transactionDateCol.setCellValueFactory(transaction -> new SimpleObjectProperty(transaction.getValue().getDate().toLocalDate()));
+        transactionAccountCol.setCellValueFactory(transaction -> new SimpleIntegerProperty(customer.getAccounts().get(0).getAccountNumber()).asObject());
+
+        transactionHistory.setItems(FXCollections.observableList(customer.getAccounts().get(0).getTransactions()));
     }
 
     private void populateWithdrawalAccountSelector() {
@@ -103,7 +119,17 @@ public class AccountsOverviewController {
 
     @FXML
     private void withdrawMoney(ActionEvent actionEvent) {
+        int withdrawalAmmount = Integer.parseInt(withdrawalAmountField.getText());
 
+        if (withdrawalAmountField.getText().isEmpty()) {
+            CustomerMain.showErrorMessage("Ogiltig inmatning", "Felaktig inmatning");
+        } else if (withdrawalAmmount <= 0) {
+            CustomerMain.showErrorMessage("Ogiltig inmatning", "Du kan inte mata in ett negativt tal");
+        } else {
+            AccountRepository.withdraw(withdrawalAccountSelector.getSelectionModel().getSelectedItem().getAccountId(), withdrawalAmmount);
+            populateAccountsOverview();
+            populateWithdrawalAccountSelector();
+        }
     }
 
     @FXML
